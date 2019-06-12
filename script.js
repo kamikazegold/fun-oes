1fd97b1aedcd0e957401e7e467936fc6020a2978
matheus <mathesumsantos15@gmail.com>
2019-06-12 16:41:19 -0300

    funções

---
+++ script.js
@@ -0,0 +1,52 @@
+//let materialescada = prompt("digite o material da escada");
+//let degrau = materialescada;
+//let qntd= Number(prompt('o tche, quantos degris guri'));
+
+//for(let i=0; i<qntd; i++){
+  //  console.log(materialescada);
+   // materialescada +=degraus;
+//}
+
+
+//let filmes = [
+//    {titulo:"toy story 4", idade:0, },
+  //  {titulo:'jhon wick 3', idade:16, },
+    //{titulo:"a forma da agua", idade:16, },
+   // {titulo: "um sonho de liberdade", idade:16,}
+//];
+
+//let idade = prompt("digite a sua idade");
+
+
+// funções niga ESTADA MLK PRA VC SER ALGUEM NA VIDA
+//function abriralert(){
+//    alert( 'kamikaze');
+//}
+//let idade = prompt("qual sua idade");
+//if(idade< 16){
+//    abriralert();
+//}
+
+//function somar(x,y , z){
+    //console.log(x + y + z);
+//}
+//somar(9, 10 , 90);
+
+//function somar(n1, n2, n3, n4){
+  //  let media = (n1+n2+n3+n4)/4;
+//    console.log(media)
+//}
+
+//somar(7, 9, 5, 10,)
+
+
+   
+
+
+
+
+
+
+
+    
+
